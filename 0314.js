023424d28b1a38367defdeb9d26eebf74d21f067
LeftDong <95334185+Leftdong@users.noreply.github.com>
2022-03-14 17:48:47 +0900

    Add files via upload

---
+++ 0314.js
@@ -0,0 +1,18 @@
+$(function(){
+  //////////////////////////////////////////
+  $('.mainSlide').slick({
+    arrows:false,
+    autoplay: true,
+    autoplaySpeed:1000,
+    dots:true,
+  });
+
+  $('.mainVisual .slideArrows i:first-child').on('click', function() {
+    $('.mainSlide').slick('slickPrev')
+  });
+  
+  $('.mainVisual .slideArrows i:last-child').on('click', function() {
+    $('.mainSlide').slick('slickNext')
+  });
+/////////////////////////////////////////////////////
+});
